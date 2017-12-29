8327c35659c6965761d3b59216c20d962c055348
Matthew Lee <matthew.lee711@gmail.com>
2017-12-29 17:29:29 -0600

    Send message to people in the same room

--- server/server.js
+++ server/server.js
@@ -36,14 +36,21 @@ io.on('connection', (socket) => {
 
   // Sent from client to server to client
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+
+    if(user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
     // Optional -- allows user to know server received message
     callback('Received');
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+    var user = users.getUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude))
+    }
   });
 
   socket.on('disconnect', () => {
